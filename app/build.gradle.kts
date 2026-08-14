389a5312490b0601f4dd5b2b1e18ca467f914019
SimplePasserby <alx@eclipso.ch>
2026-08-14 06:41:28 +0300

    Update build.gradle.kts
    vers!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.duck.photopicker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "0.66"
     }
 	
